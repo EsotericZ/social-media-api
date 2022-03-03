d5666d6ec5f5f09f4b1b18f6d3d62cb0f54f4259
EsotericZ <cjsand03@gmail.com>
2022-03-02 17:44:53 -0700

    added routes

--- routes/api/thought/index.js
+++ routes/api/thought/index.js
@@ -1,3 +1,18 @@
 const router = require('express').Router();
+const { createThought,
+    getAllThoughts,
+    getThoughtById,
+    updateThoughtById,
+    deleteThoughtById
+ } = require('../../../controllers/thoughtController');
+
+router.route('/')
+    .post(createThought)
+    .get(getAllThoughts);
+
+router.route('/:thoughtId')
+     .get(getThoughtById)
+     .put(updateThoughtById)
+     .delete(deleteThoughtById);
 
 module.exports = router;
--- routes/api/user/index.js
+++ routes/api/user/index.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
-const { createUser } = require('../../../controllers/userController');
+const { createUser,
+        getAllUsers,
+        getUserById,
+        updateUserById,
+        deleteUserById
+     } = require('../../../controllers/userController');
 
 router.route('/')
-    .post(createUser);
+    .post(createUser)
+    .get(getAllUsers);
+
+router.route('/:userId')
+     .get(getUserById)
+     .put(updateUserById)
+     .delete(deleteUserById);
 
 module.exports = router;
